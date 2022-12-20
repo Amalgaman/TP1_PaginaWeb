a8ac42c7a46759c481591bdb5171ac8d604895c2
ArdussoNahuel <nahuelardusso@gmail.com>
2022-12-19 22:02:22 -0300

    dsadasd
    adasda

---
+++ conf/conf.php
@@ -0,0 +1,10 @@
+<?php
+
+define('BASE_URL', 'http://localhost/clase15/');
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'clase15');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+
+?>
---
+++ layout/_css.php
@@ -0,0 +1,3 @@
+<link href="<?php echo BASE_URL ?>css/bootstrap.css" rel="stylesheet" />
+<link href="<?php echo BASE_URL ?>css/styles.css" rel="stylesheet" />
+<script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
