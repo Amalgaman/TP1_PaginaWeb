1a701c85ee332952dd3f7ee76018df83005e34c3
ArdussoNahuel <nahuelardusso@gmail.com>
2022-12-19 22:02:06 -0300

    dasdsad
    adasda

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define ('BASE_URL', 'http://localhost/TP1_PaginaWeb/');
-
-define ('DB_HOST', 'localhost');
-define ('DB_NAME', 'juegos_tp');
-define ('DB_USER', 'root');
-define ('DB_PASSWORD', '');
-?>
